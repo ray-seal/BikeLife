9d433db485556fb8fd8a64c89ba755f5c3d3bab9
ray-seal <rayseal1986@gmail.com>
2025-07-01 04:04:57 +0100

    Update pages.tsx

--- src/app/pages.tsx
+++ src/app/pages.tsx
@@ -5,7 +5,7 @@ const Map = dynamic(() => import('@/components/Map'), { ssr: false });
 export default function Home() {
   return (
     <main className="min-h-screen p-8">
-    <h1 className="text-4x1 font-bold mb-6"> Bike-Life UK</h1>
+    <h1 className="text-4x1 font-bold mb-6"> 🏍️Bike-Life UK🏍️</h1>
     <p className=mb-4 text-gray-700">Discover riders near you.</p>
     <div className="border rounded-md overflow-hidden h-[500px]">
     <Map />
