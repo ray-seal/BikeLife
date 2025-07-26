1000801e17e0b8e5a7704851a20c690644ca339e
Matt Sheldon <44905900+mattsheldon212@users.noreply.github.com>
2025-07-26 21:37:36 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
-import AuthPage from './pages/AuthPage';
-import CallbackPage from './pages/CallbackPage';
+// import AuthPage from './pages/AuthPage'; //uncomment this when built
+// import CallbackPage from './pages/CallbackPage'; //uncomment this when built
 import { NotFoundPage } from './pages/NotFoundPage';
 // Import ThemeSwitcher if used
 // import ThemeSwitcher from './path-to-ThemeSwitcher';
@@ -13,8 +13,12 @@ function App() {
       {/* <ThemeSwitcher /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
+        {/* Also uncomment Auth and Callback when they get made */}
+        {/*
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/auth/callback" element={<CallbackPage />} />
+        */}
+        
         {/* Catch-all must go last */}
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
