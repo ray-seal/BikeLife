fafad9b2d00eff51e4da92e8f6f0647dde42bdc3
ray-seal <rayseal1986@gmail.com>
2025-07-26 19:50:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import { BrowserRouter as Router,
-        Routes, Route } from 'react-router-dom'
-import { HomePage } from './pages/HomePage'
-import AuthPage from './pages/AuthPage'
-import CallbackPage from './pages/CallbackPage'
-import { NotFoundPage } from './pages/NotFoundPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+import CallbackPage from './pages/CallbackPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+// Import ThemeSwitcher if used
+// import ThemeSwitcher from './path-to-ThemeSwitcher';
 
 function App() {
   return (
     <Router>
-    <ThemeSwitcher />
+      {/* Uncomment the following line if ThemeSwitcher is imported */}
+      {/* <ThemeSwitcher /> */}
       <Routes>
-      <Route path="/"
-        element={<HomePage />} />
-        <Route path="/auth"
-          element={<AuthPage />} />
-        <Route path="auth/callback"
-          element={<CallbackPage />} />
-
-        {/* Catch-all must go last*/}
-        <Route path="*"
-          element={<NotFoundPage />} />
-              </Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/auth/callback" element={<CallbackPage />} />
+        {/* Catch-all must go last */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </Router>
-    )
+  );
+}
 
-  export default App
-    )
+export default App;
