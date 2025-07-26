914eb8f82599d791be12bd3c06ba45d93c99989e
ray-seal <rayseal1986@gmail.com>
2025-07-26 01:07:39 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router,
+        Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import AuthPage from './pages/AuthPage'
+import CallbackPage from './pages/CallbackPage'
+import { NotFoundPage } from './pages/NotFoundPage'
+
+function App() {
+  return (
+    <Router>
+    <ThemeSwitcher />
+      <Routes>
+      <Route path="/"
+        element={<HomePage />} />
+        <Route path="/auth"
+          element={<AuthPage />} />
+        <Route path="auth/callback"
+          element={<CallbackPage />} />
+
+        {/* Catch-all must go last*/}
+        <Route path="*"
+          element={<NotFoundPage />} />
+              </Routes>
+    </Router>
+    )
+
+  export default App
+    )
