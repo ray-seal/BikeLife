bf9766e2efcc3f9102860ab379ef54364ef65743
Matt Sheldon <44905900+mattsheldon212@users.noreply.github.com>
2025-07-26 22:12:11 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
-import AuthPage from './pages/AuthPage';
-import CallbackPage from './pages/CallbackPage';
+import { AuthPage } from './pages/AuthPage';
+import { CallbackPage } from './pages/CallbackPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 // Import ThemeSwitcher if used
 // import ThemeSwitcher from './path-to-ThemeSwitcher';
