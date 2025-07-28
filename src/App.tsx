15b622af99d57fcf1a42db36aefe5b2b01d993c1
ray-seal <rayseal1986@gmail.com>
2025-07-28 02:55:08 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/auth/callback" element={<CallbackPage />} />
-        <Route path="/create-profile" element={<CreateProfilePage />} /> {/* your new route */}
+        <Route path="/create-profile" element={<CreateProfilePage />}  {/* your new route */}
         <Route path="/test" element={div style={{ padding:32, fontSize: 24 }}>Test Route Works</div>} />
         {/* Catch-all must go last */}
         <Route path="*" element={<NotFoundPage />} />
