2947b851cf6cfa3dc99605f16dc1780b893c0705
ray-seal <rayseal1986@gmail.com>
2025-07-29 11:12:29 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { AuthPage } from './pages/AuthPage';
-import { CallbackPage } from './pages/CallbackPage';
-import { NotFoundPage } from './pages/NotFoundPage';
-import { CreateProfilePage } from './pages/CreateProfilePage';
-import { NewsFeedPage } from './pages/NewsFeedPage';
-import { UserProfilePage } from './pages/UserProfilePage'; // <-- Add this import
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import CallbackPage from "./pages/CallbackPage";
+import CreateProfilePage from "./pages/CreateProfilePage";
+import NewsFeedPage from "./pages/NewsFeedPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import NotFoundPage from "./pages/NotFoundPage";
+// Import Notifications bell component
+import { Notifications } from "./components/Notifications";
 
 function App() {
   return (
     <Router>
-      {/* Uncomment the following line if ThemeSwitcher is imported */}
-      {/* <ThemeSwitcher /> */}
+      {/* Top bar with Notifications bell */}
+      <div className="flex justify-end items-center p-2">
+        <Notifications />
+      </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/auth/callback" element={<CallbackPage />} />
         <Route path="/create-profile" element={<CreateProfilePage />} />
         <Route path="/news-feed" element={<NewsFeedPage />} />
-        <Route path="/profile/:user_id" element={<UserProfilePage />} /> {/* <-- Add this route */}
+        <Route path="/profile/:user_id" element={<UserProfilePage />} />
         <Route
           path="/test"
           element={<div style={{ padding: 32, fontSize: 24 }}>Test Route Works</div>}
