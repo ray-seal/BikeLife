e95fd69a352a3db8479cb5a797bfad1ce82e7c81
ray-seal <rayseal1986@gmail.com>
2025-07-29 15:56:56 +0100

    Update FollowersFeedPage.tsx

--- src/pages/FollowersFeedPage.tsx
+++ src/pages/FollowersFeedPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { createClient, User } from "@supabase/supabase-js";
 import { Post } from "../components/Post"; // Adjust path as needed
+import { supabase } from "../supabaseClient";
 
 const supabaseUrl = "https://mhovvdebtpinmcqhyahw.supabase.co/";
 const supabaseKey = "sb_publishable_O486ikcK_pFTdxn-Bf0fFw_95fcL_sP";
