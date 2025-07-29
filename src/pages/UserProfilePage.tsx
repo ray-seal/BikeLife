0f934d44b1555a482219cfef2dd5626e7bc1a1d3
ray-seal <rayseal1986@gmail.com>
2025-07-29 10:57:47 +0100

    Update UserProfilePage.tsx

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://mhovvdebtpinmcqhyahw.supabase.co/";
