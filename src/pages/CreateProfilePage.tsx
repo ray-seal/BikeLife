be497f05c5719df5b41cd9f7b2ded66c8c9b780a
ray-seal <rayseal1986@gmail.com>
2025-07-29 16:05:14 +0100

    Update CreateProfilePage.tsx

--- src/pages/CreateProfilePage.tsx
+++ src/pages/CreateProfilePage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../supabaseClient";
 
