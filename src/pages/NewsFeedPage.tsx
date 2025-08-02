b2df4bfc143a7431360fd20ef1fbf9d4710a7e53
ray-seal <rayseal1986@gmail.com>
2025-08-02 05:47:31 +0100

    Update NewsFeedPage.tsx

--- src/pages/NewsFeedPage.tsx
+++ src/pages/NewsFeedPage.tsx
@@ -336,7 +336,7 @@ export const NewsFeedPage: React.FC = () => {
           to="/followers-feed"
           className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded shadow flex items-center"
         >
-          Followers Feed
+          Followers
         </Link>
       </div>
       <br /><br /><br /><br />
